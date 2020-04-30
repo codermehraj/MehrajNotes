0360cf8b28c23b7d36f1d99a85b784779a641ce2
Md Mehrajul Islam <47929493+codermehraj@users.noreply.github.com>
2020-04-30 23:36:44 +0600

    Create 7 >> nCr .cpp

---
+++ Recursion/7 >> nCr .cpp
@@ -0,0 +1,32 @@
+/* ~ CoderMehraJ ~ */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define show(x) cout << #x << " = " << x << '\n';
+
+/*
+    We are using a combinatorics formula to calculate nCr
+    The formula is :
+        nCr = (n-1)C(r-1) + (n-1)C(r)
+        So, The recursive formula will be: 
+        nCr(n,r) = nCr(n-1,r-1) + nCr(n-1,r)
+        Lets find out :D
+        Releted Problem : UVa 369
+        Maybe time is hard !!!
+*/
+int nCr(int n, int r){
+    if(n==r) return 1;
+    if(r==1) return n;
+    return nCr(n-1,r-1)+nCr(n-1,r); 
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,r;
+    cin>>n>>r;
+    // Using recursion to get nCr
+    cout<<nCr(n,r)<<endl;
+    return 0;
+}
